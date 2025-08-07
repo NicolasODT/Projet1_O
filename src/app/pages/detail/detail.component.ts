cc478d97f032f677aa1b0e508234640dc917e1b6
nicolasODT <nicolas.terwagne@gmail.com>
2025-08-07 18:54:40 +0200

    refactor detail and home components to use observables for data handling, improve layout and styling

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,61 +1,33 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Subscription} from 'rxjs';
-import {OlympicService} from 'src/app/core/services/olympic.service';
-import {Olympic} from 'src/app/core/models/Olympic';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ChartDataService, DetailPageData } from 'src/app/core/services/chart-data.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent implements OnInit, OnDestroy {
-  public countryData: Olympic | undefined;
-  public totalMedals = 0;
-  public totalAthletes = 0;
-  public lineChartData: { name: string, series: { name: string, value: number }[] }[] = [];
-  private subscription: Subscription | undefined;
+export class DetailComponent implements OnInit {
+  public detailPageData$: Observable<DetailPageData | null> = of(null);
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private olympicService: OlympicService
+    private chartDataService: ChartDataService
   ) {}
 
   ngOnInit(): void {
-    const countryId = +this.route.snapshot.params['id']; // On récupère l'ID du pays
-
-    this.subscription = this.olympicService.getOlympics().subscribe(data => {
-      if (data) {
-        this.countryData = data.find(olympic => olympic.id === countryId);
-
-        if (this.countryData) {
-          // Calculer les totaux
-          this.totalMedals = this.countryData.participations.reduce((acc, p) => acc + p.medalsCount, 0);
-          this.totalAthletes = this.countryData.participations.reduce((acc, p) => acc + p.athleteCount, 0);
-
-          // Préparer les données pour le graphique en ligne
-          this.lineChartData = [
-            {
-              name: this.countryData.country,
-              series: this.countryData.participations.map(p => ({
-                name: p.year.toString(),
-                value: p.medalsCount
-              }))
-            }
-          ];
-        }
-      }
-    });
+    this.detailPageData$ = this.route.params.pipe(
+      switchMap(params => {
+        const countryId = +params['id'];
+        return this.chartDataService.getDetailPageData(countryId);
+      })
+    );
   }
 
   goBack(): void {
     this.router.navigateByUrl('');
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { OlympicService } from 'src/app/core/services/olympic.service';
-import { Olympic } from 'src/app/core/models/Olympic';
-import { LegendPosition } from '@swimlane/ngx-charts'; // <-- 1. IMPORTER CECI
+import { Observable, of } from 'rxjs';
+import { LegendPosition } from '@swimlane/ngx-charts';
+import { ChartDataService, HomePageData } from 'src/app/core/services/chart-data.service';
 
-// Interface pour typer les données du graphique et l'événement de sélection
-interface ChartData {
+// Interface pour l'événement de sélection
+interface ChartSelectEvent {
   name: string;
   value: number;
   extra: { id: number };
@@ -17,36 +16,18 @@ interface ChartData {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  private subscription: Subscription | undefined;
-  private olympicData: Olympic[] = [];
-  public chartData: ChartData[] = [];
-  public numberOfJos = 0;
-  public numberOfCountries = 0;
+export class HomeComponent implements OnInit {
+  public homePageData$: Observable<HomePageData | null> = of(null);
   public screenWidth!: number;
 
-  constructor(private olympicService: OlympicService, private router: Router) {}
+  constructor(
+    private chartDataService: ChartDataService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.screenWidth = window.innerWidth;
-    this.subscription = this.olympicService.getOlympics().subscribe(data => {
-      if (data) {
-        this.olympicData = data;
-        this.numberOfCountries = data.length;
-        this.chartData = data.map(olympic => ({
-          name: olympic.country,
-          value: olympic.participations.reduce((acc, curr) => acc + curr.medalsCount, 0),
-          extra: { id: olympic.id },
-        }));
-        const uniqueYears = new Set<number>();
-        data.forEach(olympic => {
-          olympic.participations.forEach(participation => {
-            uniqueYears.add(participation.year);
-          });
-        });
-        this.numberOfJos = uniqueYears.size;
-      }
-    });
+    this.homePageData$ = this.chartDataService.getHomePageData();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -54,20 +35,13 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.screenWidth = window.innerWidth;
   }
 
-  // 2. UTILISER LE TYPE ICI
   get legendPosition(): LegendPosition {
     return this.screenWidth < 768 ? LegendPosition.Below : LegendPosition.Right;
   }
 
-  onSelect(event: ChartData): void {
+  onSelect(event: ChartSelectEvent): void {
     if (event.extra && event.extra.id) {
       this.router.navigate(['/detail', event.extra.id]);
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
     }
   }
 }
